2b45dd751449ac0c9aeb836ae5272f409976fdd6
queeenraa <lauraamelia50@gmail.com>
2023-11-18 08:50:47 +0700

    first commit

---
+++ backend/create.php
@@ -0,0 +1,41 @@
+<?php
+
+require './../config/db.php';
+
+if(isset($_POST['submit'])) {
+    global $db_connect;
+
+    $name = $_POST['name'];
+    $price = $_POST['price'];
+    $image = $_FILES['image']['name'];
+    $tempImage = $_FILES['image']['tmp_name'];
+
+    $randomFilename = time().'-'.md5(rand()).'-'.$image;
+
+    $result = mysqli_query($db_connect, "SELECT MAX(id) AS max_id FROM products");
+
+    $uploadPath = __DIR__ . '/../upload/' . $randomFilename;
+    if (move_uploaded_file($tempImage, $uploadPath)) {
+        mysqli_query($db_connect, "INSERT INTO products (name,price,image)
+                    VALUES ('$name','$price','upload/$randomFilename')");
+        echo "berhasil upload";
+    } else {
+        echo "gagal upload";
+    }
+
+    
+    /// $uploadPath = $_SERVER['DOCUMENT_ROOT'].'/upload/'.$randomFilename;
+
+    
+
+   /// if($upload) {
+   ///     $upload = move_uploaded_file($tempImage,$uploadPath);
+   ///     mysqli_query($db_connect,"INSERT INTO products (name,price,image)
+   ///                 VALUES ('$name','$price','/upload/$randomFilename')");
+   ///     echo "berhasil upload";
+ ///   } else {
+   ///     echo "gagal upload";
+   /// }
+
+    ///
+}
